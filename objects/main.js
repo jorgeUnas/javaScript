cd8c5f64e409df5c65ee8763d9906c00eff0ba0d
admin <admin@admins-MacBook-Pro.local>
2024-11-04 16:10:48 +0000

    A constructor for Books

--- objects/main.js
+++ objects/main.js
@@ -12,3 +12,19 @@ function Player(name, marker) {
   const player2 = new Player('also steve', 'O');
   player1.sayName(); // logs 'steve'
   player2.sayName(); // logs 'also steve'
+
+
+  // Book constructor
+
+  function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return `${title} by ${author}, ${pages}, ${read}`;
+    }
+  }
+
+  const book1 = new Book('The Hobbie', 'Tolkien', 295, 'not read yet');
+  console.log(book1.info());
